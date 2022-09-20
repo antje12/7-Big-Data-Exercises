c85e202835dfc6cdafccf90c6cef463fca7b30da
anton <antje12@student.sdu.dk>
2022-09-20 21:34:28 +0200

    e3

--- lecture03-exercises/alice-in-kafkaland-consumer/example.py
+++ lecture03-exercises/alice-in-kafkaland-consumer/example.py
@@ -1,9 +1,17 @@
 from hdfs import InsecureClient
 from kafka import KafkaConsumer
 
-
 # Create a KafkaConsumer that consumes messages from the topic 'alice-in-kafkaland'
+consumer = KafkaConsumer('alice-in-kafkaland', bootstrap_servers=['kafka:9092'], group_id='group1', consumer_timeout_ms=10000)
 
-# Combine all the messages into a single string
+client = InsecureClient('http://namenode:9870', user='root')
 
+string = ''
+
+for msg in consumer:
+    # Combine all the messages into a single string
+    string += msg.value.decode('utf-8')
+    
 # Write the string to HDFS in a file called 'alice-in-kafkaland.txt'
+with client.write('/alice-in-kafkaland.txt', encoding='utf-8', overwrite=True) as writer:
+    writer.write(string)
--- lecture03-exercises/producer-client/example.py
+++ lecture03-exercises/producer-client/example.py
@@ -1,5 +1,12 @@
-# input
-string = str(input())
-  
-# output
-print(string)
+from kafka import KafkaProducer
+
+producer = KafkaProducer(bootstrap_servers=['kafka:9092'])
+
+while (True):
+    # input
+    string = str(input())
+
+    producer.send('foo', bytes(string, 'utf-8'))
+
+    # output
+    print(string)
