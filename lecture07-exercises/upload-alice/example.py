d4aca99ed7cab4183eb34a4f917198796d90b18c
anton <antontj@live.dk>
2022-10-25 21:57:34 +0200

    e7

--- lecture07-exercises/upload-alice/example.py
+++ lecture07-exercises/upload-alice/example.py
@@ -13,23 +13,31 @@ cur  = conn.cursor()
 
 # Your task is to replace the following code to ingest the individual words of Alice in Wonderland
 # First you need to read the file (the file is loaded into the root of the container at ./alice-in-wonderland.txt)
+with open('alice-in-wonderland.txt', mode="r", encoding="utf-8-sig") as f:
+    lines = f.read().replace("\n", " ").replace('"', "'")
 
 # Then you must split the content of the file into individual words
+words = lines.split()
 
 # Then create a table called alice and insert the individual words as (id, word) into the table
-
-
-
-## Example of interacting with the database
-query = f"CREATE TABLE IF NOT EXISTS test (a int)"
+query = f"CREATE TABLE IF NOT EXISTS alice (id int, word varchar(255));"
 cur.execute(query)
 
-PRODUCT_ID = 1201
-price = 10000
-PRODUCT_TYPE = 'PRI'
-
-query = f"INSERT INTO test (a) VALUES ('{PRODUCT_ID}')"
+query = f"DELETE FROM alice;"
 cur.execute(query)
-print(f"{cur.rowcount} details inserted")
+
+count = 0
+for index, word in enumerate(words):
+    query = f'INSERT INTO alice (id, word) VALUES ("{index+1}", "{word}");'
+    cur.execute(query)
+    count += cur.rowcount
+
+print(f"{count} words inserted")
+
+query = f"SELECT COUNT(*) FROM alice;"
+cur.execute(query)
+result = cur.fetchall()
+print(result)
+
 conn.commit()
 conn.close()
